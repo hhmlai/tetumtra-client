06ea91895ed4a3b0dbdaa4511539c23cffa8e9da
Horacio Henriques <horacio.henriques@pfmo.tl>
2020-06-01 12:29:43 +0100

    revisão texto e timeout

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ model = 'sp_int16_en'
 class TranslateForm(FlaskForm):
     pagedown = PageDownField('Escreva o texto a traduzir')
     submit = SubmitField('Traduzir')
-
+    
 translation = ''
 
 @app.route('/', methods=['GET', 'POST'])
@@ -31,26 +31,29 @@ def index():
     pair = "pttt" #Default
     if form.validate_on_submit():
         text = form.pagedown.data
-        text_list =  re.split('(\n)', text)
-        tok_list = []
-        for p in text_list:
-            if len(p)>1:
-                tok_list.append(list(intersperse(' ', nltk.sent_tokenize(p))))
-            else: 
-                tok_list.append(p)
-        text_list = [sent for par in tok_list for sent in par ]
-        pair = request.form['lang']
-        for text in text_list:
-            if len(text) > 1:
-                res = requests.post('https://server-dot-tetumtra.appspot.com/trans/', json={'model': model, 'pair': pair, 'text': text})
-                if res.ok:
-                    translation = translation + res.json()['translation']
+        if len(text) > 1000:
+            translation="Texto demasiado longo, por favor reduza para traduzir.\n\nTestu naruk tebes! Favor ida bele reduz hodi tradús."
+        else:
+            text_list =  re.split('(\n)', text)
+            tok_list = []
+            for p in text_list:
+                if len(p)>1:
+                    tok_list.append(list(intersperse(' ', nltk.sent_tokenize(p))))
+                else: 
+                    tok_list.append(p)
+            text_list = [sent for par in tok_list for sent in par ]
+            pair = request.form['lang']
+            for text in text_list:
+                if len(text) > 1:
+                    res = requests.post('https://server-dot-tetumtra.appspot.com/trans/', json={'model': model, 'pair': pair, 'text': text})
+                    if res.ok:
+                        translation = translation + res.json()['translation']
+                    else:
+                        print(res)
                 else:
-                    print(res)
-            else:
-                translation = translation + text
+                    translation = translation + text
     else:
-        form.pagedown.data = ('Teste aqui o tradutor')
+        form.pagedown.data = ('Uma experiência com "aprendizagem máquina" e tétum, mas ainda muito por fazer!!')
     return render_template('index.html', form=form, pair=pair, text=translation)
 
 if __name__ == '__main__':
